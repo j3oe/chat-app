9ab2d642098c724fcbb534648f967582826948fb
Joseph <looooooooooooooooooooooooooooooooooooooooooooool@hotmail.com>
2026-02-14 19:21:18 -0800

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ wss.on('connection', ws => {
     console.log('New client connected');
 
     ws.on('message', message => {
-        // Broadcast to all clients, including sender
+        // Broadcast to everyone
         wss.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(message.toString());
