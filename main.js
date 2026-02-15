f58426c0872159a6eecd2f4d4701db75e24811fd
Joseph <looooooooooooooooooooooooooooooooooooooooooooool@hotmail.com>
2026-02-14 19:20:20 -0800

    Update main.js

--- main.js
+++ main.js
@@ -2,24 +2,24 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 500,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
-    }
-  });
+    const win = new BrowserWindow({
+        width: 500,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,    // Allows renderer.js to use require()
+            contextIsolation: false
+        }
+    });
 
-  win.loadFile('index.html');
+    win.loadFile('index.html');
 }
 
 app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
-  if(process.platform !== 'darwin') app.quit();
+    if (process.platform !== 'darwin') app.quit();
 });
 
 app.on('activate', () => {
-  if(BrowserWindow.getAllWindows().length === 0) createWindow();
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
