6a337879c5cf0345928a5222a9c842e5fe44ea18
Joseph <looooooooooooooooooooooooooooooooooooooooooooool@hotmail.com>
2026-02-14 17:14:05 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
+const WebSocket = require('ws');
 
+const wss = new WebSocket.Server({ port: 8080 });
+
+let clients = [];
+
+wss.on('connection', ws => {
+  clients.push(ws);
+  console.log('New client connected');
+
+  ws.on('message', message => {
+    clients.forEach(client => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message.toString());
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    clients = clients.filter(client => client !== ws);
+    console.log('Client disconnected');
+  });
+});
+
+console.log('WebSocket server running on ws://localhost:8080');
